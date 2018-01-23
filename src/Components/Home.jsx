bef48798ce94fb5ab2886b495ae2c3fb0cbfc4dc
spdev <spdevuk@gmail.com>
2018-01-23 00:03:47 +0000

    social links

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,30 +2,42 @@ import React, {Component} from 'react';
 
 class Home extends Component {
 
+  twitterClick = () => {
+    window.open('https://twitter.com/DGDtheband');
+  }
+    facebookClick = () => {
+    window.open('https://www.facebook.com/DanceGavinDance');
+  }
 
-clickTwitter = () => {
-  window.location = "https://twitter.com/DGDtheband";
-}
+  instaClick = () => {
+    window.open('https://www.instagram.com/dancegavindance');
+  }
+  youtubeClick = () => {
+    window.open('https://www.youtube.com/user/riserecords/search?query=dance+gavin+dance');
+  }
+
+
+
 
   render() {
     return (
       <div className="Home">
         <div className="container">
           <div className="row" id="social-links">
-            <div onClick={this.clickTwitter} className="col-xs-3" id="twitter">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-              <p>Twitter</p>
+            <div onClick={this.twitterClick} className="col-xs-3" id="twitter">
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+              <p>Twitter </p>
             </div>
-            <div className="col-xs-3" id="facebook">
-              <i class="fa fa-facebook" aria-hidden="true"></i>
+            <div onClick={this.facebookClick} className="col-xs-3" id="facebook">
+              <i className="fa fa-facebook" aria-hidden="true"></i>
               <p>Facebook</p>
             </div>
-            <div className="col-xs-3" id="instagram">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
+            <div onClick={this.instaClick} className="col-xs-3" id="instagram">
+              <i className="fa fa-instagram" aria-hidden="true"></i>
               <p>Instagram</p>
             </div>
-            <div className="col-xs-3" id="youtube">
-              <i class="fa fa-youtube" aria-hidden="true"></i>
+            <div onClick={this.youtubeClick} className="col-xs-3" id="youtube">
+              <i className="fa fa-youtube" aria-hidden="true"></i>
                 <p>Youtube</p>
             </div>
           </div>

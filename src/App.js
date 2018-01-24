d99161685d3d2f9c5b1d5bb7e0f3468a11449f96
spdev <spdevuk@gmail.com>
2018-01-24 20:35:24 +0000

    added merch-banner files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 //Components
 import Home from './components/home';
 import NavigationBar from './components/navbar';
+import MerchBanner from './components/merchbanner';
 //CSS
 import './styles/home.css';
 import './styles/variables.css';
-import './styles/navbar/navbar.css'
+import './styles/navbar/navbar.css';
+import './styles/merchbanner/merchbanner.css';
 
 class App extends Component {
   render() {
---
+++ src/components/merchbanner.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MerchBanner extends Component {
+  render() {
+    return (
+        <div className="merch-banner container-fluid">
+        hello
+
+        </div>
+    );
+  }
+}
+
+export default MerchBanner;

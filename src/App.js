f1d37180317b2583d975d184e576ab6863e2a00d
spdev <spdevuk@gmail.com>
2018-01-24 21:47:30 +0000

    added merch bar  & changed background art

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
       <div className="App">
       <NavigationBar />
       <Home />
+      <MerchBanner />
       </div>
     );
   }
--- src/components/merchbanner.jsx
+++ src/components/merchbanner.jsx
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class MerchBanner extends Component {
   render() {
     return (
-        <div className="merch-banner container-fluid">
-        hello
+        <div>
+            <div className="merch-banner container-fluid">
+                <a href="https://merchnow.com/catalogs/dance-gavin-dance" className="btn btn-default">
+                    <p>GET MERCH HERE</p>
+                </a>
 
+            </div>
         </div>
     );
   }

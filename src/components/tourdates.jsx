2e55df665f882c803a8103e9c96f5046dcca1109
spdev <spdevuk@gmail.com>
2018-01-27 06:48:14 +0000

    can't get api

--- src/components/tourdates.jsx
+++ src/components/tourdates.jsx
@@ -1,22 +1,19 @@
 import React, {Component} from 'react';
 import Request from 'superagent';
 
-
-
 class TourDates extends Component {
     constructor(props) {
         super(props)
         this.state = {}
-    }
 
-    componentWillMount(){
-        var url="https://rest.bandsintown.com/artists/Dance%20Gavin%20Dance/events?app_id=dgdsite&date=2018-01-01%2C2018-03-03"
-        Request.get(url).then((response) => {
-            this.setState({
-                venue: response.id
-            });
-        });
-}
+
+        var api = {
+            getTourDates() {
+                var url = 'https://rest.bandsintown.com/artists/Dance%20Gavin%20Dance/events?app_id=dgdband&date=2018-01-01%2C2018-03-03'
+                return fetch(url).then((res) => res.json())
+            }
+        }
+    }
 
     render() {
         return (
@@ -29,18 +26,69 @@ class TourDates extends Component {
                     </div>
                 </div>
                 <div className="jumbotron tour-jumbo">
-                <script charSet="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a className="bit-widget-initializer" data-artist-name="Swans" data-display-local-dates="false" data-display-past-dates="true" data-auto-style="false" data-text-color="#000000" data-link-color="#2F95DE" data-popup-background-color="#FFFFFF" data-background-color="#FFFFFF" data-display-limit="15" data-link-text-color="#FFFFFF"></a>
-                    <div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
+                    </div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
+                    </div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
+                    </div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
+                    </div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
+                    </div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
+                    </div>
+                    <div className="row">
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>
+                    <div className="col-xs-4 tourdates">
+                    </div>                   
                     </div>
                 </div>
                 <div className="row jumbo-links">
                     <div id="tourshowmore" className="col-xs-12">
-                       <p> 
+                        <p>
                             VIEW FULL LIST
-                       </p>
-                    <div>
-
-                        </div>
+                        </p>
+                        <div></div>
                     </div>
 
                 </div>

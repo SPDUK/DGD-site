9cb1eab2adfdf7d87757a6f9b58e3f6a6d2a64f6
spdev <spdevuk@gmail.com>
2018-02-06 19:00:21 +0000

    finished footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,8 +13,8 @@ class Footer extends Component {
                    <div className="col-xs-4 footergrid">
                    <a href="https://github.com/SPDUK">Website designed by SPDUK <i className="fa fa-github"></i></a>
                    </div>
-                   <div className="col-xs-4 footergrid">hii</div>
-                   <div className="col-xs-4 footergrid">hiuuy</div>
+                   <div className="col-xs-4 footergrid disclaimer">All product and company names are trademarks™ or registered® trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.   -- Or something like that I don't know plz don't hurt me I just made it for fun </div>
+                   <div className="col-xs-4 footergrid"><a href="https://news.bandsintown.com/home">Band tour info supplied by Bandsintown<i className="fa fa-music"></i></a></div>
             </div>
       </div>
     );
